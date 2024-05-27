7329b5cb156ea0a4a62737213304b43724961a46
Danu-Nur <diramadhan13@gmail.com>
2024-05-27 21:43:24 +0700

    create index menu

--- resources/views/layouts/side.blade.php
+++ resources/views/layouts/side.blade.php
@@ -2,6 +2,25 @@
     <div class="nk-nav-scroll">
         <ul class="metismenu" id="menu">
             {{-- <li class="nav-label">Dashboard</li> --}}
+            {{-- MASTER DATA --}}
+            <li>
+                <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                    <i class="icon-speedometer menu-icon"></i><span class="nav-text">Master Data</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="#">Data Siswa</a></li>
+                    <li><a href="#">Data PKL</a></li>
+                    <li><a href="#">Data LOKASI PKL</a></li>
+                    <li><a href="#">Data JADWAL</a></li>
+                    <li><a href="#">Data ABSENSI</a></li>
+                    <li><a href="#">Data KEGIATAN PKL</a></li>
+                    <li><a href="#">Data PENILAIAN</a></li>
+                    <li><a href="#">Data LAPORAN</a></li>
+                </ul>
+            </li>
+            {{-- END MASTER DATA --}}
+
+            {{-- MENU UTAMA --}}
             <li>
                 <a class="has-arrow" href="javascript:void()" aria-expanded="false">
                     <i class="icon-speedometer menu-icon"></i><span class="nav-text">Dashboard</span>
