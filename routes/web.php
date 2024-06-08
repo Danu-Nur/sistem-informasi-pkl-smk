edae9f2b9af30858373ae8941c9579d8e60010bd
Danu-Nur <diramadhan13@gmail.com>
2024-06-08 22:57:02 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AbsensiController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DaftarPklController;
 use App\Http\Controllers\JadwalController;
 use App\Http\Controllers\KegiatanController;
 use App\Http\Controllers\LaporanController;
@@ -28,7 +29,7 @@ Route::get('/', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
-// ROUTE SUPER ADMIN
+// ROUTE ADMIN
 Route::group(['middleware' => ['admin'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
     Route::get('dashboard', [AdminController::class, 'index'])->name('dashboard');
     Route::resource('siswa', SiswaController::class);
@@ -41,11 +42,19 @@ Route::group(['middleware' => ['admin'], 'prefix' => 'admin', 'as' => 'admin.'],
     Route::resource('laporan', LaporanController::class);
 });
 
-// ROUTE ADMIN
+// END ROUTE ADMIN
 
 
 // ROUTE SISWA
-
+Route::group(['middleware' => ['siswa'], 'prefix' => 'siswa', 'as' => 'siswa.'], function () {
+    Route::get('daftar',[DaftarPklController::class, 'index'])->name('daftar.index');
+    Route::resource('siswa', SiswaController::class);
+    Route::resource('jadwal', JadwalController::class);
+    Route::resource('absensi', AbsensiController::class);
+    Route::resource('kegiatan', KegiatanController::class);
+    Route::resource('laporan', LaporanController::class);
+});
+// END ROUTE SISWA
 
 // ROUTE PEMBIMBING SEKOLAH
 Route::group(['middleware' => ['psekolah'], 'prefix' => 'psekolah', 'as' => 'psekolah.'], function () {
@@ -53,9 +62,15 @@ Route::group(['middleware' => ['psekolah'], 'prefix' => 'psekolah', 'as' => 'pse
     Route::resource('absensi', AbsensiController::class);
     Route::resource('nilai', NilaiController::class);
 });
+// END ROUTE PEMBIMBING SEKOLAH
 
 // ROUTE PEMBIMBING INDUSTRI
-
+Route::group(['middleware' => ['pindustri'], 'prefix' => 'pindustri', 'as' => 'pindustri.'], function () {
+    // Route::get('dashboard', [AdminController::class, 'index'])->name('dashboard');
+    Route::resource('absensi', AbsensiController::class);
+    Route::resource('nilai', NilaiController::class);
+});
+// END ROUTE PEMBIMBING INDUSTRI
 
 // Route::get('/', function () {
 //     return view('welcome');
